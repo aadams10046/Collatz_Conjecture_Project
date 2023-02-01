3bed1fd9a273b886a0a37d23552fa66a165fdbdd
Alexander Adams <120673750+aadams10046@users.noreply.github.com>
2023-02-01 13:19:40 -0500

    Update collatz.py
    Commenting the code for readability.

--- collatz.py
+++ collatz.py
@@ -1,8 +1,9 @@
-import matplotlib.pyplot as plt
+#Importing the necessary libraries for the task
 import seaborn as sns
 import pandas as pd
 import numpy as np
 
+#Defining a function to check that the numbers meet the criteria of the Collatz conjecture, then find the number of iterations of Collatz are necessary
 def collatz_test(num):
     if num <= 0:
         print(f'{num} is not a valid input.\nPlease choose a number greater than or equal to 1.')
@@ -20,6 +21,7 @@ def collatz_test(num):
                 collatz_num += 1
         return collatz_num
 
+#Defining a function that plots the numbers and the number of iterations for each number from a start_num to an end_num using seaborn
 def seaborn_plot(start_num,end_num):
     if int(start_num)<=0 or end_num<=0:
         pass
@@ -32,4 +34,5 @@ def seaborn_plot(start_num,end_num):
         plt.title('Number vs. Number of Collatz Iterations')
         plt.show()
 
+#Using seaborn_plot to create a scatterplot of the results for numbers 1 through 100000
 seaborn_plot(1, 100000)
